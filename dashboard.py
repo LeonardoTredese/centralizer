fe6e877ffe4e25f57971d56184bbc17a6f24343d
Leonardo Tredese <leonardo.tredese@lnl.infn.it>
2020-03-06 15:24:07 +0100

    Second commit :)

---
+++ dashboard.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+import json 
+import requests as req
+
+app=Flask(__name__)
+remotes_list= ['10.6.0.41:5000']
+
+@app.route('/')
+def hello():
+	m = []
+	for remote in remotes_list:
+		sys_info = json.loads(req.get('/'.join(['http:/',remote,'sys'])).text)
+		iocs_info = json.loads(req.get('/'.join(['http:/',remote,'list'])).text)
+		m.append({'sys': sys_info, "iocs": iocs_info})
+	u = {
+		"bootstrap_css": url_for('static', filename="bootstrap.min.css"),
+		"bootstrap_js": url_for('static', filename="bootstrap.min.js"),
+		"jquery_js": url_for('static', filename="jquery.min.js"),
+	}
+	return render_template('index.html', machines=m, urls=u)
+
