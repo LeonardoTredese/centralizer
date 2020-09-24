e4fa3fe8bb16644239c5f4b094f39754dbc2cdb8
LeonardoTredese <leonardo.tredese@hotmail.com>
2020-09-24 09:10:03 +0200

    Aggiornata la pagina web per essere compatibile con il nuovo api, non è ancora supportato il multithreading

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from remote import Remote
 
 app=Flask(__name__)
@@ -10,28 +10,28 @@ remotes_list = [
 remotes_list = { arg[0]:Remote(*arg) for arg in remotes_list}
 
 @app.route('/')
-def hello():
-	return {'remotes': list(remotes_list.keys())}
+def index():
+	return render_template('index.html', machines = { host_name : list(remote.services.keys()) for host_name, remote in remotes_list.items()})
+
+@app.route('/remotes')
+def remotes():
+	return { host_name : list(remote.services.keys()) for host_name, remote in remotes_list.items()}
 
 @app.route('/<remote_host>/<service_name>/start')
 def start_service(remote_host, service_name):
-	return { service_name: remotes_list[remote_host].start_service(service_name)}
+	return { 'start' : remotes_list[remote_host].start_service(service_name)}
 
 @app.route('/<remote_host>/<service_name>/stop')
 def stop_service(remote_host, service_name):
-	return { service_name: remotes_list[remote_host].stop_service(service_name)}
+	return { 'stop' : remotes_list[remote_host].stop_service(service_name)}
 
 @app.route('/<remote_host>/<service_name>/status')
 def status_service(remote_host, service_name):
-	return { service_name: remotes_list[remote_host].status_service(service_name)}
-
-@app.route('/<remote_host>/list')
-def list_remote_services(remote_host):
-	return { 'services': list(remotes_list[remote_host].services.keys())}
+	return { 'status' : remotes_list[remote_host].status_service(service_name)}
 
 @app.route('/<remote_host>/sys')
 def system_remote_info(remote_host):
 	return remotes_list[remote_host].stats()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=False)
