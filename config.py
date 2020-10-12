3a86467b3feab279b4c14065511128c652f418bb
Leonardo <leonardo.tredese@lnl.infn.it>
2020-10-12 15:44:58 +0200

    Uncommited changes, del push precedente

--- config.py
+++ config.py
@@ -1,25 +1,29 @@
 from configparser import ConfigParser
 from os import listdir
-from os.path import isfile, join
+from os.path import isdir, expanduser, isfile, join
 
 
 
-def get_configuration(directory= "/usr/share/.centralizer/config/"):
-	parser = ConfigParser()
-	for configFile in listdir(directory):
-		configFile = join(directory, configFile)
-		if isfile(configFile):
-			parser.read(configFile)
-	return parser
+def get_configuration(directory= expanduser("~/.centralizer/config/")):
+    parser = ConfigParser()
+    if not isdir(directory):
+        print('[+] This is not a directory: ', directory)
+        print('[+] Not importing anything')
+    else:    
+        for configFile in listdir(directory):
+            configFile = join(directory, configFile)
+            if isfile(configFile):
+                parser.read(configFile)
+    return parser
 
 def extract_args(parser):
-	arguments = list()
-	for name in parser.sections():
-		config = parser[name]
-		if 'hostname' in config and 'username' in config and 'password' in config:
-			arguments.append((name, 
-							  config['hostname'],
-							  config['username'],  
-							  config['password'], 
-							  config.get('use_sudo', 'no') == 'yes'))
-	return arguments
+    arguments = list()
+    for name in parser.sections():
+        config = parser[name]
+        if 'hostname' in config and 'username' in config and 'password' in config:
+            arguments.append((name,
+                            config['hostname'],
+                            config['username'],
+                            config['password'],
+                            config.get('use_sudo', 'no') == 'yes'))
+    return arguments
