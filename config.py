f60fa7aa08f9fcb5ddbd92538ac4720c578af9eb
Leonardo <leonardo.tredese@lnl.infn.it>
2020-10-27 13:13:10 +0100

    Added meaninfl comments to the methods

--- config.py
+++ config.py
@@ -5,6 +5,14 @@ from os.path import isdir, expanduser, isfile, join
 
 
 def get_configuration(directory= expanduser("~/.centralizer/config/")):
+    '''Prepares a parser with all the config files contained in a directory
+
+        Args:
+            directory: the directory where the configuration files are stored. str or unicode, default: ~/.centralizer/config/
+
+        Returns:
+            A parser that contains all the file in the given directory. ConfigParser
+    '''
     parser = ConfigParser()
     print(directory)
     if not isdir(directory):
@@ -18,6 +26,18 @@ def get_configuration(directory= expanduser("~/.centralizer/config/")):
     return parser
 
 def extract_args(parser):
+    '''Extracts the information to build remotes from a given parser
+
+        Args:
+            parser: the parser with configuration file information. ConfigParser
+
+        Returns:
+            a list of tuples with the following stucture (name_of_the_remote,
+                                                            hostname,
+                                                            username,
+                                                            password,
+                                                            use_sudo)
+    '''
     arguments = list()
     for name in parser.sections():
         config = parser[name]
